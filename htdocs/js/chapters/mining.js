28f54bce5b24155102f55fc637ad4a8287e2fa4b
PrototypeAlex <darth.gibson@gmail.com>
2011-09-15 16:01:06 +1200

    Mining Changes

--- htdocs/js/chapters/mining.js
+++ htdocs/js/chapters/mining.js
@@ -4,8 +4,12 @@ var growth, oilExp, coalExp, metalExp, oilCounter, coalCounter, metalCounter;
 $(document).ready( function() {
 	$('#bubble-chart').remove();
 	createMiningSliders();
-	createMiningChart();
 	createMiningCounters();
+	createMiningChart();
+	
+	$('#coal-slider').slider({ value: 0.015 });
+	$('#oil-slider').slider({ value: 0.015 });
+	$('#metal-slider').slider({ value: 0.015 });
 });
 
 function createMiningSliders() {
@@ -17,10 +21,8 @@ function createMiningSliders() {
 			max: 0.1,
 			step: .01,
 			slide: function( event, ui ) {
-				$('#current-tourists').html(thousands(Math.round(x(ui.value))));
 				growth = getMiningGrowth(ui.value, $('#coal-slider').slider('value'), $('#metal-slider').slider('value'));
 				lineChart.refresh(growth);
-				refreshMiningCounters();
 			}
 	});
 	$('#coal-slider').slider({
@@ -28,10 +30,8 @@ function createMiningSliders() {
 		max: 0.1,
 		step: .01,
 		slide: function( event, ui ) {
-			$('#current-tourists').html(thousands(Math.round(x(ui.value))));
 			growth = getMiningGrowth($('#oil-slider').slider('value'), ui.value, $('#metal-slider').slider('value'));
 			lineChart.refresh(growth);
-			refreshMiningCounters();
 		}
 	});
 	$('#metal-slider').slider({
@@ -39,14 +39,10 @@ function createMiningSliders() {
 		max: 0.1,
 		step: .01,
 		slide: function( event, ui ) {
-			$('#current-tourists').html(thousands(Math.round(x(ui.value))));
 			growth = getMiningGrowth($('#oil-slider').slider('value'), $('#coal-slider').slider('value'), ui.value);
 			lineChart.refresh(growth);
-			refreshMiningCounters();
 		}
 	});
-	
-	
 	
 	var chart = d3.selectAll(".mining-slider-legend")
 	    .append("svg:svg")
@@ -74,7 +70,7 @@ function createMiningSliders() {
 
 function createMiningChart() {
 	lineChart = new LineChart('mining-chart');
-	growth = getMiningGrowth(0, 0, 0);
+	growth = getMiningGrowth(0.015, 0.015, 0.015);
 	lineChart.CreateLineChart(growth, 960, 235);
 }
 
@@ -99,19 +95,17 @@ function getMiningGrowth(oil, coal, metal) {
 		var miningObj = {'year': year, 'oil': oil, 'coal': coal, 'metal': metal};	
 		miningGrowth.push(miningObj);
 	}
+	refreshMiningCounters();
+	
 	return miningGrowth;
 	
 }
 
 function createMiningCounters() {
 	oilCounter = new flipCounter('oil-counter', {value:2011, inc:10, pace:50, auto:true});
-	oilCounter.incrementTo(oilExp);
-	
 	coalCounter = new flipCounter('coal-counter', {value:2254, inc:10, pace:50, auto:true});
-	coalCounter.incrementTo(coalExp);
-	
 	metalCounter = new flipCounter('metal-counter', {value:2011, inc:10, pace:50, auto:true});
-	metalCounter.incrementTo(metalExp);
+	
 }
 
 function refreshMiningCounters() {
